b8c09a69871962254f354e1dfa3b363d7c36dca7
mickasabbah <45943721+mickasabbah@users.noreply.github.com>
2019-01-09 16:57:45 +0200

    Update IDal.cs

--- DAL/IDal.cs
+++ DAL/IDal.cs
@@ -10,23 +10,27 @@ namespace DAL
     public interface IDal
     {
         
+        #region tester function
         bool AddTester(Tester tester);
         bool RemoveTester(string testerID);
         bool UpdateTester(Tester tester);
-
-
+        #endregion
+            
+        #region trainee function
         bool AddTrainee(Trainee trainee);
         bool RemoveTrainee(string traineeID);
         bool UpdateTrainee(Trainee trainee);
 
-
+        #region drivingtest function
         bool AddDrivingTest(Test test);
         bool RemovedrivingTest(int serialNumber);
         bool UpdateDrivingTest(Test test);
-
+        #endregion
+        
+        #region getting list function
         List<Tester> GetTesters(Predicate<Tester> predicate = null);
         List<Trainee> GetTrainees(Predicate<Trainee> predicate = null);
         List<Test> GetTests(Predicate<Test> predicate = null);
-
+        #endregion
     }
 }
