b5f5aea5b75bacd3a9c137968907d2b7f4a72b0d
mickasabbah <45943721+mickasabbah@users.noreply.github.com>
2019-01-10 18:05:51 +0200

    Update IBl.cs

--- BL/IBl.cs
+++ BL/IBl.cs
@@ -9,22 +9,28 @@ namespace BL
 {
     public interface IBl
     {
+        #region tester function
         bool AddTester(Tester tester);
         bool RemoveTester(string testerID);
         bool UpdateTester(Tester tester);
-
-
+        #endregion
+        
+        #region trainee function
         bool AddTrainee(Trainee trainee);
         bool RemoveTrainee(string traineeID);
         bool UpdateTrainee(Trainee trainee);
-
-
+        #endregion
+        
+        #region test function
         bool AddDrivingTest(Test test);
         bool RemovedrivingTest(int serialNumber);
         bool UpdateDrivingTest(Test test);
-
+        #endregion
+           
+        #region getting list function
         List<Tester> GetTesters(Predicate<Tester> predicate = null);
         List<Trainee> GetTrainees(Predicate<Trainee> predicate =null);
         List<Test> GetTests(Predicate<Test> predicate = null);
+        #endregion
     }
 }
